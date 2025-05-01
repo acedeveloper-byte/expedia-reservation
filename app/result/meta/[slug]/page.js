fdb73e96742c52695177884d5c93ba6dbd671e6e
veerrai <karamveer20000625@gmail.com>
2025-05-01 09:02:58 +0530

    api fetch

--- app/result/meta/[slug]/page.js
+++ app/result/meta/[slug]/page.js
@@ -1,12 +1,10 @@
 'use client'
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import { useSearchParams } from 'next/navigation';
 import React from 'react'
 
 const page = async ({ params}) => {
     const pas = await params
-    console.log(pas)
        
 
   return (
